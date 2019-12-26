0c2a43c55435df5ae459fd1e7523fd2ff226b24f
root <root@localhost.localdomain>
2019-12-26 23:04:24 +0100

    Added functions to main.py and sol_framework, and added links to settings: - Added exams management functionality

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ UnipgSolBot
 Author: Lotus
 """
 import logging
-from colorama import Fore, Style
 
-from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatAction,
+                      InlineKeyboardButton, InlineKeyboardMarkup)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler, PicklePersistence)
-from settings import LOGIN_URL, MAIN_URL, TOKEN
+                          ConversationHandler, PicklePersistence, CallbackQueryHandler)
+from settings import *
 import sol_framework as sol
 import requests
 from bs4 import BeautifulSoup
@@ -23,29 +23,40 @@ logger = logging.getLogger(__name__)
 pp = PicklePersistence(filename='u_data.pickle', on_flush=True)
 
 def start(update, context):
-	start_msg = "*Benvenuto a* @Sol_pg_Bot.\n"\
-		"Questo bot ti permetterà di visualizzare, iscriverti gli appelli disponibili e quelli a cui ti sei già iscritto sulla piattaforma SOL Unipg.\n\n"\
+	start_msg = "*Benvenuto a* @Sol\_pg\_Bot.\n"\
+		"Questo bot ti permetterà di visualizzare gli appelli disponibili e quelli a cui ti sei già iscritto sulla piattaforma SOL Unipg.\n"\
+        "Ti permetterà inoltre di iscriverti a nuovi appelli se disponibili\n\n"\
+        "Per visualizzare i comandi disponibili utilizza /help\n\n"\
 		"NOTA: _Questo bot non è reallizzato ufficialmente, pertanto né Unipg né Cineca sono da considerarsi responsabili di questo bot in alcun modo._"
 
-	update.message.reply_text(start_msg)
+	update.message.reply_markdown(start_msg, reply_markup=ReplyKeyboardRemove())
 	pp.flush()
 
+def help(update, context):
+	help_msg = "Ecco una lista dei *comandi* attualmente disponibili su questo bot:\n\n"\
+        "- /help: Visualizza questo messaggio\n"\
+        "- /cancel: Interrompe il comando attualmente in esecuzione\n"\
+        "- /login: Effettua il Login sul portale SOL chiedendo le credenziali\n"\
+        "- /logout: Cancella ogni dato personale raccolto dal Bot, compresa la sessione corrente ed effettuando quindi il Logout dal portale\n"\
+        "- /yourExams: Visualizza gli esami a cui sei iscritto\n"\
+        "- /viewExams: Visualizza gli esami disponibili e ti permette eventualmente di iscriverti\n"
+
+	update.message.reply_markdown(help_msg)
+
 def login(update, context):
-	send_user_pwd = 'Inserisci il tuo *username* e la tua *password* nel seguente formato (con un solo spazio in mezzo):\n\n'\
-                    'username password\n\n'\
+	send_user_pwd = 'Inserisci il tuo *username* e la tua *password* nel seguente formato:\n\n'\
+                    'username:password\n\n'\
                     '_Si ricorda che il bot cancellerà immediatamente il messaggio inviato non appena sarà stato effettuato il login, per questioni di Privacy._'
 
 	update.message.reply_markdown(send_user_pwd)
 	return 1
 
-def login_1(update, context):
+def login_handler(update, context):
 	# Save credentials
 	context.user_data['credentials'] = {}
-	print(context.user_data)
 
     # Getting username and password from message's text
-	user_pass = update.message.text.split(' ')
-	print(user_pass)
+	user_pass = update.message.text.split(':')
 
     # Delete user message
 	context.bot.delete_message(chat_id=update.effective_message.chat_id, message_id=update.message.message_id)
@@ -68,22 +79,80 @@ def login_1(update, context):
 
 	# Getting Name and Surname of the user just to show that login was performed correctly
 	main_page = context.user_data['session'].get(MAIN_URL)
-	soup= BeautifulSoup(main_page.text)
+	soup= BeautifulSoup(main_page.text, features='lxml')
 	name = soup.body.find('div', attrs={'class':'masthead_usermenu_user_name'}).text
-	update.message.reply_markdown("Sono riuscito a collegarmi, benvenuto *%s*!" %name )
+	update.message.reply_markdown("Sono riuscito a collegarmi, benvenuto *%s*!" % name)
 
 	# Update pickle file
 	pp.flush()
 
 	return ConversationHandler.END
 
-def help(update, context):
-	help_msg = "Questa è una lista degli attuali *comandi* presenti nel bot:\n\n"\
-		"- /help: Visualizza questo messaggio\n"\
-		"- /login: Effettua il Login sul portale SOL chiedendo le credenziali\n"\
-		"- /logout: Cancella ogni dato personale raccolto dal Bot, compresa la sessione corrente ed effettuando quindi il Logout dal portale\n"\
+def view_enrolled_exams(update, context):
+    # Verifica che l'utente abbia effettuato l'accesso
+    response = sol.connect(context.user_data)
+    if response != "OK":
+        update.message.reply_markdown(response, reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
 
-	update.message.reply_markdown(help_msg)
+    booked = sol.getEnrolledExams(context.user_data)
+    update.message.reply_markdown('Ecco gli appelli a cui sei iscritto:\n\n%s' %booked)
+    return ConversationHandler.END
+
+def view_exams(update, context):
+    # Verifica che l'utente abbia effettuato l'accesso
+    response = sol.connect(context.user_data)
+    if response != "OK":
+        update.message.reply_markdown(response, reply_markup=ReplyKeyboardRemove())
+        return ConversationHandler.END
+    # Inizializza Inline Keyboard
+    keyboard = [[ InlineKeyboardButton("Visualizza", callback_data='0'),
+         InlineKeyboardButton("Iscriviti", callback_data='1') ]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    view_msg = "Vuoi solo visualizzare gli appelli disponibili o vuoi anche iscriverti?\n"#\
+        #"_Puoi interrompere qualunque comando in esecuzione con il comando /cancel_\n"
+    update.message.reply_text(view_msg, reply_markup=reply_markup)
+    return 1
+
+def view_exams_handler(update, context):
+    # Esegue la funzione di sol_framework
+    exams = sol.getExams(context.user_data)
+    update.message.reply_markdown('Ecco gli appelli disponibili:\n\n%s' %exams, reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
+def enroll_exams_handler(update, context):
+    # Esegue la funzione di sol_framework
+    exams, links_list = sol.getExams(context.user_data)
+    update.message.reply_markdown('Ecco gli appelli disponibili:\n'\
+        'Quelli a cui puoi iscriverti sono segnati con %s, quelli a cui non puoi con %s \n\n%s\n' %type_to_sym['Prenotazione'] %type_to_sym['Vietato'] %exams, reply_markup=ReplyKeyboardRemove())
+    update.message.reply_markdown('Gli appelli disponibili sono segnati con un numero.\n'\
+        'Inserisci il numero dell\'appello a cui vuoi iscriverti, senza spazi, o inserisci 0 se vuoi annullare l\'iscrizione\n')
+    answer = int(update.message.text)
+    if answer in range(len(links_list)+1):
+        if answer == 0:
+            return ConversationHandler.END
+        else:
+            answer -= 1
+            res = sol.enrollToExam(context.user_data, links_list[answer])
+            if res == 'OK':
+                update.message.reply_markdown('\n*Prenotazione effettuata con successo*\n')
+                return ConversationHandler.END
+            else:
+                update.message.reply_markdown(res, reply_markup=ReplyKeyboardRemove())
+                ConversationHandler.END
+
+def logout(update, context):
+    # Delete all
+    for key in list(context.user_data):  # Using a list to prevent RuntimeError, since user_data could change during iterations
+        del context.user_data[key]
+
+    # Update pickle file
+    pp.flush()
+
+    # Notification message
+    update.message.reply_markdown("Tutti i dati che erano presenti sono stati rimossi con successo.\n\n")
+    return ConversationHandler.END
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -92,38 +161,48 @@ def error(update, context):
 def cancel(update, context):
     # Undo any command which is going on
     update.message.reply_text('Ok, comando annullato.', reply_markup=ReplyKeyboardRemove())
-
+    return ConversationHandler.END
 
 def main():
-	# Create EventHandler with bot's token
-	updater = Updater(TOKEN, persistence=pp, use_context=True)
-	dp = updater.dispatcher
+    # Create EventHandler with bot's token
+    updater = Updater(TOKEN, persistence=pp, use_context=True)
+    dp = updater.dispatcher
 
 	# Adding all the Handlers for the commands
-	cmd_login = ConversationHandler(
+    cmd_login = ConversationHandler(
         entry_points=[CommandHandler('login', login)],
 
         states={
-            1: [MessageHandler(Filters.text, login_1)]
+            1: [MessageHandler(Filters.text, login_handler)]
         },
 
-        fallbacks=[CommandHandler('cancel', cancel)]
-    )
+        fallbacks=[CommandHandler('cancel', cancel)])
 
-	dp.add_handler(CommandHandler('start', start))
-	dp.add_handler(CommandHandler('help', help))
-	dp.add_handler(CommandHandler('cancel', cancel))
-	dp.add_handler(cmd_login)
+    cmd_view_exams= ConversationHandler(
+        entry_points=[CommandHandler('viewExams', view_exams)],
 
-	# Log all errors
-	dp.add_error_handler(error)
+        states={
+            1: [CallbackQueryHandler(view_exams_handler, pattern='^0$'),
+                CallbackQueryHandler(enroll_exams_handler, pattern='^1$')]
+        },
 
-	# Starts the bot.
-	updater.start_polling()
+        fallbacks=[CommandHandler('cancel', cancel)])
 
-	# Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
-	updater.idle()
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+    dp.add_handler(CommandHandler('logout', logout))
+    dp.add_handler(CommandHandler('cancel', cancel))
+    dp.add_handler(CommandHandler('yourExams', view_enrolled_exams))
+    dp.add_handler(cmd_login)
+    dp.add_handler(cmd_view_exams)
 
+    # Log all errors
+    dp.add_error_handler(error)
+    # Starts the bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT.
+    updater.idle()
 
 if __name__=='__main__':
 	main()
--- sol_framework.py
+++ sol_framework.py
@@ -3,8 +3,7 @@ Functions to communicate with SOL platform
 """
 import requests
 from bs4 import BeautifulSoup
-from colorama import Fore, Style
-from settings import LOGIN_URL, MAIN_URL, HOME_URL
+from settings import *
 
 def connect(user_data):
     if 'session' not in user_data:
@@ -12,14 +11,14 @@ def connect(user_data):
 
     # Getting credentials if available
     try:
-        auth = (user_data['credentials']['username'], user_data['credentials']['password'])
+        user_data['session'].auth = (user_data['credentials']['username'], user_data['credentials']['password'])
     except KeyError:
         return "Non hai effettuato ancora il login, effettualo tramite il comando /login."
 
     # Check if server is alive
     status_code = requests.head(HOME_URL).status_code
     if status_code != 200:
-        print(Fore.RED + "[CONNECTION] Server irraggiungibile. Status code: " + str(status_code))
+        print("[CONNECTION] Server irraggiungibile. Status code: " + str(status_code))
         return "Non riesco a contattare il server (CODE: %d), riprova più tardi..." % status_code
 
     # Check if the login is still valid (without performing a login)
@@ -27,7 +26,75 @@ def connect(user_data):
         return "OK"
 
     # Perform the login
-    user_data['session'].get(LOGIN_URL)
-    if user_data['session'].get(LOGIN_URL, auth=auth).url != MAIN_URL:
+    if user_data['session'].get(LOGIN_URL).url != MAIN_URL:
         return "Le credenziali fornite non sono valide. Riprova."
     return "OK"
+
+def getEnrolledExams(user_data):
+    # Gets the hmtl page with the data
+    response = user_data['session'].get(ENROLLED_EXAMS_URL)
+    soup = BeautifulSoup(response.text, features='lxml')
+    booked = soup.body.find_all('table', class_='detail_table')
+
+    # Parsing data, creating a list with final output
+    out = []
+    for i in range(len(booked)):
+        temp=[]
+        for j in booked[i].get_text().split('\n'):
+            if j!='':
+                temp.append(j+'\n')
+        out.extend((temp[0], temp[-1]))
+
+    for i in range(len(out)):
+        if i%2:
+            out[i]='Data: ' + ' '.join([out[i][:10], 'Ora:', out[i][10:15]]) + '\n\n'
+        else:
+            out[i]=type_to_sym['Prenotazione']+out[i]
+
+    result = ''
+    for i in out:
+        result += i
+    return result
+
+def getExams(user_data):
+    # Gets the hmtl page with the data
+    response = user_data['session'].get(EXAMS_URL)
+    soup = BeautifulSoup(response.text, features='lxml')
+    exams = soup.body.table('td')
+
+    # Parsing data and creating an element in a list for each exam
+    exams = [exams[6*i:6*(i+1)]  for i in range(int(len(exams)/6))]
+
+    result=''
+    linksList=[]
+    count=0
+    for i in exams:
+        if(i[0].div):
+            count+=1
+            linksList.append(i[0].div.a['href'])
+            result+=str(count)+' '+type_to_sym['Prenotazione']
+        else:
+            result+=type_to_sym['Vietato']
+        for j in range(1,5):
+            result+=('', i[j].get_text()+' ')[j==1]
+            result+=('', 'Data: '+i[j].get_text())[j==2]
+            result+=('', 'Iscrizioni dal: '+i[j].get_text()[0:10]+' al: '+i[j].get_text()[10:20])[j==3]
+            result+=('', '\nDescrizione: '+ i[j].get_text())[j==4]
+        result+='\n\n'
+
+    return result, linksList
+
+def enrollToExam(user_data, link):
+    # Funzione per realizzare la registrazione ad un appello
+    response = user_data['session'].get(BASE_URL+link)
+    soup = BeautifulSoup(response.text, features='lxml')
+    params = soup.body.form('input')
+    en_link = soup.body.form['action']
+    payload = {i['name']:i['value'] for i in params}
+    enroll = user_data['session'].post(BASE_URL+en_link, data=payload)
+    soup2 = BeautifulSoup(enroll.text)
+    res = soup2.body('p', class_="app-text_esito_pren_msg-esito_pren")
+    if res == 'PRENOTAZIONE EFFETTUATA\n':
+        return 'OK'
+    else:
+        return 'ERROR - La prenotazione *non* è stata effettuata con successo, riprova più tardi'
